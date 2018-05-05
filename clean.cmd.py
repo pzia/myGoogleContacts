0b1d693f62a9556512f5b0c60f3753546d64864d
pzia <paparazzia@gmail.com>
2018-05-05 21:28:07 +0200

    some commands

---
+++ clean.cmd.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import lib
+
+lib.commandGeneric(lib.match_brackets, lib.match_void)
---
+++ thunderdbird.cmd.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import lib
+import sys
+
+lib.commandImportThund(sys.argv[1])
