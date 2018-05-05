59cd644f96f9d4174c5f67ab273ab22e20723936
pzia <paparazzia@gmail.com>
2018-05-05 21:18:25 +0200

    anon configuration

---
+++ myconf.dist.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+client_id='somethinglikeakey12345.apps.googleusercontent.com'
+client_secret='sometext'
+scope='https://www.googleapis.com/auth/contacts'
+user_agent='MyGoogleContact/0.1'
